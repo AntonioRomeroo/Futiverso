ff941b18bf69b0744c4000a83243fc25663ebbc0
Farlopio-ctrl <antonioromeroperez2000@gmail.com>
2026-04-27 20:26:55 +0200

    Archivo laliga eliminado

--- resources/views/laliga.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="wrap content">
-        <h1>La Liga</h1>
-        <p>Bienvenido a la categoría de camisetas de La Liga.</p>
-    </div>
-@endsection
